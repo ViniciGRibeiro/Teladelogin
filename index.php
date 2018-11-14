7f8c8a8a2010d1053b3187f5072322fbe5254709
ViniciGRibeiro <viniciusgomesribeiro99@gmail.com>
2018-11-14 16:44:34 -0200

    Tela de login com html e css

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Tela de Login</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    </head>
+    <body>
+        
+        <div class="login-box">
+            <h1>Login</h1>
+            <div class="textbox">
+                <i class="fas fa-user"></i>
+                <input type="text" placeholder="Username" name="" value="">
+            </div>
+            
+            <div class="textbox">
+                <i class="fas fa-lock"></i>
+                <input type="password" placeholder="Password" name="" value="">
+            </div>
+            
+            <input class="btn" type="button" name="" value="Sign">
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
